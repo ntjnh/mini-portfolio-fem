0baabe813f0fdbc1b47a003322823a34dfad5c6a
Nate J.M <nate@natejonah.com>
2024-04-17 17:58:45 +0100

    Add required Google fonts to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,29 @@
 module.exports = {
   siteMetadata: {
     title: `mini-portfolio-fem`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Open Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&display=swap`,
+          },
+          {
+            name: `Lora`,
+            file: `https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
 };
