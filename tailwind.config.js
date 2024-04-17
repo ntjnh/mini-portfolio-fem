88cd3592e637b715fad11e2e8dabd6af92994815
Nate J.M <nate@natejonah.com>
2024-04-17 17:00:10 +0100

    Set new container sizes in tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '880px',
+      'xl': '986px',
+      '2xl': '1110px'
+    },
     extend: {},
   },
   plugins: [],
