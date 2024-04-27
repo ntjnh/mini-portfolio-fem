77be6225e5544111fdd7b76ee3a06da98e3a2a51
Nate J.M <nate@natejonah.com>
2024-04-27 22:42:54 +0100

    Start building out Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const Contact = () => {
     return (
         <Layout>
-            <h1 className="text-center">Contact</h1>
+            <div className="flex">
+                <div className="w-1/3">
+                    <h1 className="text-3xl">Get In Touch</h1>
+                </div>
+
+                <div className="w-2/3">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eaque molestias quis esse voluptatibus. Suscipit asperiores repellendus officia distinctio nam, excepturi sunt quibusdam itaque exercitationem omnis iste earum debitis eius.</p>
+                </div>
+            </div>
         </Layout>
     )
 }
+
+export const Head = () => <Seo title="Contact Me" />
 
 export default Contact
