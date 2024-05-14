b1aeb289dc919a9044c6318fa6ae825c21e0fc38
Nate J.M <nate@natejonah.com>
2024-05-14 21:41:16 +0100

    Start styling projects on the Portfolio page

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -7,23 +7,25 @@ import Seo from '../../components/seo'
 const Portfolio = ({ data }) => {
     return (
         <Layout>
-            <h1 className="mb-6 text-center text-4xl">Portfolio</h1>
-
-            <section className="py-12">
+            <section className="pt-12">
                 {
                     data.allMdx.nodes.map(node => {
                         const thumb = getImage(node.frontmatter.thumbnail)
 
                         return (
-                            <article className="flex mb-16" key={node.id}>
-                                <div className="w-1/2">
-                                    <GatsbyImage image={thumb} alt={`${node.frontmatter.title} thumbnail`} />
+                            <article className="flex mb-16 project-item" key={node.id}>
+                                <div className="image-col w-1/2">
+                                    <GatsbyImage class="w-[96%]" image={thumb} alt={`${node.frontmatter.title} thumbnail`} />
                                 </div>
 
-                                <div className="w-1/2">
-                                    <h2 className="mb-4 text-3xl">{node.frontmatter.title}</h2>
-                                    <p className="mb-6">{node.frontmatter.description}</p>
-                                    <Link className="border border-slate-950 inline-block leading-none px-6 py-4 font-semibold text-xs tracking-widest uppercase" to={`/portfolio/${node.frontmatter.slug}`}>View Project</Link>
+                                <div className="text-col w-1/2">
+                                    <div className="border-b border-t border-slate-300 flex flex-wrap h-full items-center text-col-inner w-4/5">
+                                        <div>
+                                            <h2 className="mb-10 text-4.5xl text-slate-700">{node.frontmatter.title}</h2>
+                                            <p className="font-medium leading-8 mb-6 text-slate-600 text-sm">{node.frontmatter.description}</p>
+                                            <Link className="border border-slate-700 font-bold inline-block leading-none px-6 py-4 text-slate-700 text-xs tracking-widest uppercase" to={`/portfolio/${node.frontmatter.slug}`}>View Project</Link>
+                                        </div>
+                                    </div>
                                 </div>
                             </article>
                         )
@@ -31,7 +33,7 @@ const Portfolio = ({ data }) => {
                 }
             </section>
 
-            <section className="py-36">
+            <section className="pt-20 pb-36">
                 <div className="flex items-center justify-between">
                     <div className="w-[30%]">
                         <h2 className="leading-[2.75rem] text-4xl">Interested in doing a project together?</h2>
@@ -58,7 +60,7 @@ export const query = graphql`
                     slug
                     date(formatString: "MMMM D, YYYY")
                     thumbnail {
-                        childrenImageSharp {
+                        childImageSharp {
                             gatsbyImageData
                         }
                     }
